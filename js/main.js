1af55cd6eea3b00130a10ddab7d4935fadd025a0
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-05-09 18:08:11 +0200

    [UPD] milestone 3

--- js/main.js
+++ js/main.js
@@ -7,10 +7,11 @@ console.log('main.js caricato');
 const operando = document.getElementById('elem');
 let operando1;
 let operando2;
-let currentOperator;
+let currentOperator="";
 
 const numbers = document.querySelectorAll('.number');
 const operators = document.querySelectorAll('.operator');
+const equalOperator = document.querySelector('.equal');
 
 // Aggiungere event listener a tutti i numeri (0-9):
 for (let i = 0; i < numbers.length; i++){
@@ -22,12 +23,23 @@ for (let i = 0; i < numbers.length; i++){
 // aggiungere event listener a tutti gli operatori
 for (let i = 0; i < operators.length; i++){
     operators[i].addEventListener("click", function (){
-        operando1=operando.innerText;
+        // salvere il primo operando
+        operando1=parseInt(operando.innerText);
+        // resettare il numero in alto
         operando.innerText = "";
+        // salvare l'operatore cliccato
         selectOperator(operators[i].innerText);
 
     })
 }
+
+// aggiungere event listener al pulsante "=": quando si clicca su =
+equalOperator.addEventListener("click", function(){
+    // salvare il secondo operando
+    operando2=parseInt(operando.innerText);
+    // visualizzare il risultato in alto
+    operando.innerText = calculate();
+})
 
 function selectNumber(selectedNumber){
     // quando si clicca su un numero, concatenarlo al numero visualizzato in alto
@@ -39,4 +51,19 @@ function selectOperator(selectedOperator){
     // quando si clicca su un operatore, salvare l'operatore cliccato e il primo operando, quindi resettare il numero in alto
     currentOperator=selectedOperator;
 
+}
+
+function calculate() {
+    // effetuare il calcolo corretto in base all'operatore
+    switch(currentOperator){
+        case "+":
+            return operando1+operando2;
+        case "-":
+            return operando1-operando2;
+        case "x":
+            return operando1*operando2;
+        case "/":
+        if(operando2===0) return "ERROREEEEEEE";
+        else return operando1/operando2;
+    }
 }
