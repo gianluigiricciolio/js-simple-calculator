9cb7469fac43317c32ea9a07532c571dd9c671b4
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-05-09 17:44:28 +0200

    [UPD] added milestone 2

--- README.md
+++ README.md
@@ -2,7 +2,8 @@ La nostra calcolatrice deve fare dei semplici calcoli tra due numeri interi.
 Quindi l'utente inserisce le cifre del primo numero, dopo dovrà cliccare sull'operazione, poi inserirà le cifre del secondo numero e infine dovrà cliccare sul tasto = per vedere il risultato. Non si può applicare più di un'operazione alla volta.
 
 MILESTONE 1
-Aggiungere event listener a tutti i numeri (0-9): quando si clicca su un numero, concatenarlo al numero visualizzato in alto
+Aggiungere event listener a tutti i numeri (0-9):
+quando si clicca su un numero, concatenarlo al numero visualizzato in alto
 
 MILESTONE 2
 aggiungere event listener a tutti gli operatori: quando si clicca su un operatore, salvare l'operatore cliccato e il primo operando, quindi resettare il numero in alto
--- js/main.js
+++ js/main.js
@@ -5,16 +5,38 @@ console.log('main.js caricato');
 // NUMERO VISUALIZZATO SULLA CALCOLATRICE
 
 const operando = document.getElementById('elem');
+let operando1;
+let operando2;
+let currentOperator;
 
 const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+
+// Aggiungere event listener a tutti i numeri (0-9):
 for (let i = 0; i < numbers.length; i++){
     numbers[i].addEventListener("click", function (){
         selectNumber(numbers[i].innerText);
     })
 }
 
+// aggiungere event listener a tutti gli operatori
+for (let i = 0; i < operators.length; i++){
+    operators[i].addEventListener("click", function (){
+        operando1=operando.innerText;
+        operando.innerText = "";
+        selectOperator(operators[i].innerText);
+
+    })
+}
 
 function selectNumber(selectedNumber){
+    // quando si clicca su un numero, concatenarlo al numero visualizzato in alto
     if(operando.innerText=="0") operando.innerText = selectedNumber;
     else operando.innerText += selectedNumber;
+}
+
+function selectOperator(selectedOperator){
+    // quando si clicca su un operatore, salvare l'operatore cliccato e il primo operando, quindi resettare il numero in alto
+    currentOperator=selectedOperator;
+
 }
